82437dc658a9e958fd9f97fa8cd73801bd3d5faf
Pubudu W <pubudu.wijesinghe@levylab.org>
2021-11-22 11:02:10 -0500

    2D Voltage Calib Script (Draft) added

--- pysrc/misc/plot_volt_calib.py
+++ pysrc/misc/plot_volt_calib.py
@@ -2,22 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+pixel_range = [150,400]
+
 path = "C:\\Users\\Pubudu Wijesinghe\\Desktop\\SLM_VC_Data"
 os.chdir(path)
 cnt = 0
+data = []
 
 for file in os.listdir():
     if file.endswith(".txt"):
         cnt += 1
         datafile = f"{path}\{file}"
         with open(datafile,'r') as datafile:
-            data = np.genfromtxt(datafile, delimiter='\t')
-        imgname = file.split(".")[0]
-        plt.figure()
-        plt.title(file)
-        # plt.imshow(np.transpose(data))
-        plt.plot(data)
-        plt.savefig(f"data{imgname}")
-        print(f"{cnt} files done")
-        # plt.show()
+            datalist = np.genfromtxt(datafile, delimiter='\t').T
+            datalist = datalist[pixel_range[0]:pixel_range[1]]
+            data.append(datalist)
+    print("file done")
+
+flat_data = [item for sublist in data for item in sublist]
+
+fig = plt.figure()
+ax = plt.gca()
+plt.imshow(flat_data)
+ax.axes.yaxis.set_visible(False)
+plt.show()
+
 
