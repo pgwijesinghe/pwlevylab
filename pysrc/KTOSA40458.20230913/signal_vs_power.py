b83e05b7662d6caf6e9295c3811ee91c6799fd49
Pubudu W (PHOTON) <pubudu.wijesinghe@levylab.org>
2024-02-23 10:42:55 -0500

    mend

--- pysrc/KTOSA40458.20230913/signal_vs_power.py
+++ pysrc/KTOSA40458.20230913/signal_vs_power.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 
-matplotlib.style.use('seaborn')
+matplotlib.style.use('ggplot')
 
 power_array = np.array([0.01e-6, 0.3e-6, 0.8e-6, 1.3e-6, 1.6e-6])
 signal_array = np.array([0.56e-6, 1.2e-6, 1.6e-6, 1.9e-6, 2.2e-6])
